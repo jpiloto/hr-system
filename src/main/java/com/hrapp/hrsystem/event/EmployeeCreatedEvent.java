b5733c703e768efca7dd5d546cac410bb7493ef8
pilot <pilot9280ster@gmail.com>
2025-10-14 17:03:42 -0400

    Fix: align content of EmployeeCreatedEvent with PayrollRecordDTO

--- src/main/java/com/hrapp/hrsystem/event/EmployeeCreatedEvent.java
+++ src/main/java/com/hrapp/hrsystem/event/EmployeeCreatedEvent.java
@@ -5,13 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeCreatedEvent {
     private Long employeeId;
-    private Long departmentId;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
     private LocalDate hireDate;
+    private Long departmentId;
+    private Set<Long> roleIds;
+    private String jobPositionTitle;
+
 }
 
